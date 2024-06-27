a07c008954038535c2bfd25b22d6d0976e9c10d3
WATA-Yiju <yiju.dev@watanow.com>
2024-06-27 16:11:25 +0900

    add: zustand test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,56 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import App from './App';
 
 React;
 
-describe('test code', () => {
+const context = describe;
+describe('App', () => {
   it('3 + 5 should be 8', () => {
     expect(3 + 5).toBe(8);
   });
 
-  it('renders App', () => {
-    render(<App />);
+  context('Dom Testing', () => {
+    it('renders App', () => {
+      render(<App />);
 
-    expect(screen.getByText(/App/)).toBeInTheDocument();
+      expect(screen.getByText(/App/)).toBeInTheDocument();
+    });
   });
 
-  it('renders firstName', async () => {
-    render(<App />);
+  context('MSW Testing', () => {
+    it('renders firstName', async () => {
+      render(<App />);
 
-    await waitFor(() => {
-      const firstName = screen.getByText(/John/);
-      expect(firstName);
+      await waitFor(() => {
+        const firstName = screen.getByText(/John/);
+        expect(firstName);
+      });
+    });
+  });
+
+  context('Zustand Testing', () => {
+    beforeEach(() => {
+      render(<App />);
+    });
+
+    it('should render with initial state of 0', async () => {
+      expect(await screen.findByText(/0/)).toBeInTheDocument();
+
+      const incBtn = await screen.findByRole('button', { name: /increase/i });
+
+      expect(incBtn).toBeInTheDocument();
+    });
+
+    it('should increase count by clicking a button', async () => {
+      expect(await screen.findByText(/0/)).toBeInTheDocument();
+
+      const incBtn = await screen.findByRole('button', { name: /increase/i });
+
+      fireEvent.click(incBtn);
+
+      expect(await screen.findByText(/1/)).toBeInTheDocument();
     });
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,3 +2,5 @@ import '@testing-library/jest-dom';
 import { server } from './mocks/node';
 
 server.listen();
+
+vi.mock('zustand'); // to make it work like Jest (auto-mocking)
